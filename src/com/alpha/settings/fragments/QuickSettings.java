8e36a327b408e92ea57008a144ae43c304a6f144
Marko Man <darkobas@gmail.com>
2019-10-08 11:03:31 +0200

    Add QS Header Image customization [2/2]
    neobuddy89: Massive clean up.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/QuickSettings.java
+++ src/com/alpha/settings/fragments/QuickSettings.java
@@ -37,6 +37,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.alpha.settings.fragments.quicksettings.QsHeaderImageSettings;
 import com.alpha.settings.preferences.CustomSeekBarPreference;
 
 import android.provider.Settings;
@@ -208,6 +209,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.Secure.QS_SHOW_AUTO_BRIGHTNESS, 1, UserHandle.USER_CURRENT);
         updateQsStyle(mContext);
         updateQsPanelStyle(mContext);
+        QsHeaderImageSettings.reset(mContext);
     }
 
     private void updateAnimTileStyle(int tileAnimationStyle) {
