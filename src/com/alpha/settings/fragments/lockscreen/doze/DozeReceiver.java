crdroidandroid/android_packages_apps_crDroidSettings

Allow tuning ambient display with sensors [2/3]

src/com/crdroid/settings/fragments/ui/doze/DozeReceiver.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2017-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.ui.doze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DozeReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "crDroidDoze";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            if (DEBUG) Log.d(TAG, "Starting service");
+            Utils.enableService(context);
+        }
+    }
+}

src/com/crdroid/settings/fragments/ui/doze/DozeService.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2017-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.ui.doze;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+public class DozeService extends Service {
+    private static final String TAG = "crDroidDozeService";
+    private static final boolean DEBUG = false;
+
+    private TiltSensor mTiltSensor;
+    private PickupSensor mPickupSensor;
+    private ProximitySensor mProximitySensor;
+
+    private boolean mTiltSensorAvailable;
+    private boolean mPickupSensorAvailable;
+    private boolean mProximitySensorAvailable;
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                onDisplayOn();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                onDisplayOff();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+
+        mTiltSensorAvailable = Utils.getTiltSensor(this);
+        mPickupSensorAvailable = Utils.getPickupSensor(this);
+        mProximitySensorAvailable = Utils.getProximitySensor(this);
+
+        if (!mTiltSensorAvailable && !mPickupSensorAvailable && !mProximitySensorAvailable) return;
+
+        if (mTiltSensorAvailable) mTiltSensor = new TiltSensor(this);
+        if (mPickupSensorAvailable) mPickupSensor = new PickupSensor(this);
+        if (mProximitySensorAvailable) mProximitySensor = new ProximitySensor(this);
+
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        super.onDestroy();
+
+        if (!mTiltSensorAvailable && !mPickupSensorAvailable && !mProximitySensorAvailable) return;
+
+        this.unregisterReceiver(mScreenStateReceiver);
+        if (mTiltSensorAvailable) {
+            mTiltSensor.disable();
+        }
+        if (mPickupSensorAvailable) {
+            mPickupSensor.disable();
+        }
+        if (mProximitySensorAvailable) {
+            mProximitySensor.disable();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void onDisplayOn() {
+        if (DEBUG) Log.d(TAG, "Display on");
+        if (mTiltSensorAvailable && Utils.tiltEnabled(this)) {
+            mTiltSensor.disable();
+        }
+        if (mPickupSensorAvailable && Utils.pickUpEnabled(this)) {
+            mPickupSensor.disable();
+        }
+        if (mProximitySensorAvailable && (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this))) {
+            mProximitySensor.disable();
+        }
+    }
+
+    private void onDisplayOff() {
+        if (DEBUG) Log.d(TAG, "Display off");
+        if (mTiltSensorAvailable && Utils.tiltEnabled(this)) {
+            mTiltSensor.enable();
+        }
+        if (mPickupSensorAvailable && Utils.pickUpEnabled(this)) {
+            mPickupSensor.enable();
+        }
+        if (mProximitySensorAvailable && (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this))) {
+            mProximitySensor.enable();
+        }
+    }
+}

src/com/crdroid/settings/fragments/ui/doze/ProximitySensor.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2017-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.ui.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.UserHandle;
+import android.os.Vibrator;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ProximitySensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ProximitySensor";
+
+    // Maximum time for the hand to cover the sensor: 1s
+    private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
+
+    // Minimum time until the device is considered to have been in the pocket: 2s
+    private static final int POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+    private ExecutorService mExecutorService;
+
+    private boolean mSawNear = false;
+    private long mInPocketTime = 0;
+
+    private Vibrator mVibrator;
+
+    public ProximitySensor(Context context) {
+        mContext = context;
+        mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mExecutorService = Executors.newSingleThreadExecutor();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        if (mVibrator == null || !mVibrator.hasVibrator()) {
+            mVibrator = null;
+        }
+    }
+
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean isNear = event.values[0] < mSensor.getMaximumRange();
+        if (mSawNear && !isNear) {
+            if (shouldPulse(event.timestamp)) {
+                Utils.launchDozePulse(mContext);
+                doHapticFeedback();
+            }
+        } else {
+            mInPocketTime = event.timestamp;
+        }
+        mSawNear = isNear;
+    }
+
+    private boolean shouldPulse(long timestamp) {
+        long delta = timestamp - mInPocketTime;
+
+        if (Utils.handwaveGestureEnabled(mContext) && Utils.pocketGestureEnabled(mContext)) {
+            return true;
+        } else if (Utils.handwaveGestureEnabled(mContext)) {
+            return delta < HANDWAVE_MAX_DELTA_NS;
+        } else if (Utils.pocketGestureEnabled(mContext)) {
+            return delta >= POCKET_MIN_DELTA_NS;
+        }
+        return false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        submit(() -> {
+            mSensorManager.registerListener(this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        });
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
+    }
+
+    private void doHapticFeedback() {
+        if (mVibrator == null) {
+            return;
+        }
+        int val = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.DOZE_GESTURE_VIBRATE, 0, UserHandle.USER_CURRENT);
+        if (val > 0) {
+            mVibrator.vibrate(val);
+        }
+    }
+}

src/com/crdroid/settings/fragments/ui/doze/TiltSensor.java
@@ -1,0 +1,114 @@
+/*
+ * Copyright (C) 2017-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.ui.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.os.Vibrator;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class TiltSensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "TiltSensor";
+
+    private static final int BATCH_LATENCY_IN_MS = 100;
+    private static final int MIN_PULSE_INTERVAL_MS = 2500;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+    private ExecutorService mExecutorService;
+
+    private long mEntryTimestamp;
+
+    private Vibrator mVibrator;
+
+    public TiltSensor(Context context) {
+        mContext = context;
+        mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
+        mExecutorService = Executors.newSingleThreadExecutor();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        if (mVibrator == null || !mVibrator.hasVibrator()) {
+            mVibrator = null;
+        }
+    }
+
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: " + event.values[0]);
+
+        long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return;
+        } else {
+            mEntryTimestamp = SystemClock.elapsedRealtime();
+        }
+
+        if (event.values[0] == 1) {
+            Utils.launchDozePulse(mContext);
+            doHapticFeedback();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        submit(() -> {
+            mSensorManager.registerListener(this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL, BATCH_LATENCY_IN_MS * 1000);
+            mEntryTimestamp = SystemClock.elapsedRealtime();
+        });
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
+    }
+
+    private void doHapticFeedback() {
+        if (mVibrator == null) {
+            return;
+        }
+        int val = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.DOZE_GESTURE_VIBRATE, 0, UserHandle.USER_CURRENT);
+        if (val > 0) {
+            mVibrator.vibrate(val);
+        }
+    }
+}

AlphaDroid-Project/packages_apps_AlphaSettings

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/alpha/settings/fragments/lockscreen/doze/DozeReceiver.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2017-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alpha.settings.fragments.lockscreen.doze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DozeReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "crDroidDoze";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            if (DEBUG) Log.d(TAG, "Starting service");
+            Utils.enableService(context);
+        }
+    }
+}

src/com/alpha/settings/fragments/lockscreen/doze/DozeService.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2017-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alpha.settings.fragments.lockscreen.doze;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+public class DozeService extends Service {
+    private static final String TAG = "crDroidDozeService";
+    private static final boolean DEBUG = false;
+
+    private TiltSensor mTiltSensor;
+    private PickupSensor mPickupSensor;
+    private ProximitySensor mProximitySensor;
+
+    private boolean mTiltSensorAvailable;
+    private boolean mPickupSensorAvailable;
+    private boolean mProximitySensorAvailable;
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                onDisplayOn();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                onDisplayOff();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+
+        mTiltSensorAvailable = Utils.getTiltSensor(this);
+        mPickupSensorAvailable = Utils.getPickupSensor(this);
+        mProximitySensorAvailable = Utils.getProximitySensor(this);
+
+        if (!mTiltSensorAvailable && !mPickupSensorAvailable && !mProximitySensorAvailable) return;
+
+        if (mTiltSensorAvailable) mTiltSensor = new TiltSensor(this);
+        if (mPickupSensorAvailable) mPickupSensor = new PickupSensor(this);
+        if (mProximitySensorAvailable) mProximitySensor = new ProximitySensor(this);
+
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        super.onDestroy();
+
+        if (!mTiltSensorAvailable && !mPickupSensorAvailable && !mProximitySensorAvailable) return;
+
+        this.unregisterReceiver(mScreenStateReceiver);
+        if (mTiltSensorAvailable) {
+            mTiltSensor.disable();
+        }
+        if (mPickupSensorAvailable) {
+            mPickupSensor.disable();
+        }
+        if (mProximitySensorAvailable) {
+            mProximitySensor.disable();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void onDisplayOn() {
+        if (DEBUG) Log.d(TAG, "Display on");
+        if (mTiltSensorAvailable && Utils.tiltEnabled(this)) {
+            mTiltSensor.disable();
+        }
+        if (mPickupSensorAvailable && Utils.pickUpEnabled(this)) {
+            mPickupSensor.disable();
+        }
+        if (mProximitySensorAvailable && (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this))) {
+            mProximitySensor.disable();
+        }
+    }
+
+    private void onDisplayOff() {
+        if (DEBUG) Log.d(TAG, "Display off");
+        if (mTiltSensorAvailable && Utils.tiltEnabled(this)) {
+            mTiltSensor.enable();
+        }
+        if (mPickupSensorAvailable && Utils.pickUpEnabled(this)) {
+            mPickupSensor.enable();
+        }
+        if (mProximitySensorAvailable && (Utils.handwaveGestureEnabled(this) ||
+                Utils.pocketGestureEnabled(this))) {
+            mProximitySensor.enable();
+        }
+    }
+}

src/com/alpha/settings/fragments/lockscreen/doze/ProximitySensor.java
@@ -1,0 +1,129 @@
+/*
+ * Copyright (C) 2017-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alpha.settings.fragments.lockscreen.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ProximitySensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ProximitySensor";
+
+    // Maximum time for the hand to cover the sensor: 1s
+    private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
+
+    // Minimum time until the device is considered to have been in the pocket: 2s
+    private static final int POCKET_MIN_DELTA_NS = 2000 * 1000 * 1000;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+    private ExecutorService mExecutorService;
+
+    private boolean mSawNear = false;
+    private long mInPocketTime = 0;
+
+    private Vibrator mVibrator;
+
+    public ProximitySensor(Context context) {
+        mContext = context;
+        mSensorManager = mContext.getSystemService(SensorManager.class);
+        final boolean wakeup = context.getResources().getBoolean(com.android.internal.R.bool.config_deviceHaveWakeUpProximity);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, wakeup);
+        mExecutorService = Executors.newSingleThreadExecutor();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        if (mVibrator == null || !mVibrator.hasVibrator()) {
+            mVibrator = null;
+        }
+    }
+
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean isNear = event.values[0] < mSensor.getMaximumRange();
+        if (mSawNear && !isNear) {
+            if (shouldPulse(event.timestamp)) {
+                Utils.launchDozePulse(mContext);
+                doHapticFeedback();
+            }
+        } else {
+            mInPocketTime = event.timestamp;
+        }
+        mSawNear = isNear;
+    }
+
+    private boolean shouldPulse(long timestamp) {
+        long delta = timestamp - mInPocketTime;
+
+        if (Utils.handwaveGestureEnabled(mContext) && Utils.pocketGestureEnabled(mContext)) {
+            return true;
+        } else if (Utils.handwaveGestureEnabled(mContext)) {
+            return delta < HANDWAVE_MAX_DELTA_NS;
+        } else if (Utils.pocketGestureEnabled(mContext)) {
+            return delta >= POCKET_MIN_DELTA_NS;
+        }
+        return false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        submit(() -> {
+            mSensorManager.registerListener(this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        });
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
+    }
+
+    private void doHapticFeedback() {
+        if (mVibrator == null) {
+            return;
+        }
+        int val = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.DOZE_GESTURE_VIBRATE, 0, UserHandle.USER_CURRENT);
+        if (val > 0) {
+            mVibrator.vibrate(VibrationEffect.createOneShot(val,
+                    VibrationEffect.DEFAULT_AMPLITUDE));
+        }
+    }
+}

src/com/alpha/settings/fragments/lockscreen/doze/TiltSensor.java
@@ -1,0 +1,116 @@
+/*
+ * Copyright (C) 2017-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alpha.settings.fragments.lockscreen.doze;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class TiltSensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "TiltSensor";
+
+    private static final int BATCH_LATENCY_IN_MS = 100;
+    private static final int MIN_PULSE_INTERVAL_MS = 2500;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+    private ExecutorService mExecutorService;
+
+    private long mEntryTimestamp;
+
+    private Vibrator mVibrator;
+
+    public TiltSensor(Context context) {
+        mContext = context;
+        mSensorManager = mContext.getSystemService(SensorManager.class);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
+        mExecutorService = Executors.newSingleThreadExecutor();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        if (mVibrator == null || !mVibrator.hasVibrator()) {
+            mVibrator = null;
+        }
+    }
+
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (DEBUG) Log.d(TAG, "Got sensor event: " + event.values[0]);
+
+        long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
+        if (delta < MIN_PULSE_INTERVAL_MS) {
+            return;
+        } else {
+            mEntryTimestamp = SystemClock.elapsedRealtime();
+        }
+
+        if (event.values[0] == 1) {
+            Utils.launchDozePulse(mContext);
+            doHapticFeedback();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        submit(() -> {
+            mEntryTimestamp = SystemClock.elapsedRealtime();
+            mSensorManager.registerListener(this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL, BATCH_LATENCY_IN_MS * 1000);
+        });
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
+    }
+
+    private void doHapticFeedback() {
+        if (mVibrator == null) {
+            return;
+        }
+        int val = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.DOZE_GESTURE_VIBRATE, 0, UserHandle.USER_CURRENT);
+        if (val > 0) {
+            mVibrator.vibrate(VibrationEffect.createOneShot(val,
+                    VibrationEffect.DEFAULT_AMPLITUDE));
+        }
+    }
+}