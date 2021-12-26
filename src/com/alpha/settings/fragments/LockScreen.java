crdroidandroid/android_packages_apps_crDroidSettings

Add Lockscreen album art filter [2/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/LockScreen.java
@@ -113,6 +113,8 @@ public class LockScreen extends SettingsPreferenceFragment
         Settings.Global.putInt(resolver,
                 Settings.Global.LOCKSCREEN_ENABLE_QS, 1);
         Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_ALBUMART_FILTER, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.FACE_AUTO_UNLOCK, mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_face_unlock_enabled_by_default) ? 1 : 0,
                 UserHandle.USER_CURRENT);

AlphaDroid-Project/packages_apps_AlphaSettings

src/com/alpha/settings/fragments/LockScreen.java
@@ -68,6 +68,8 @@ public class LockScreen extends SettingsPreferenceFragment
                 Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_ALBUMART_FILTER, 0, UserHandle.USER_CURRENT);
     }
 
     @Override