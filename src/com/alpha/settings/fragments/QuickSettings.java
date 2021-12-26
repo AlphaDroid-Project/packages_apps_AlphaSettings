crdroidandroid/android_packages_apps_crDroidSettings

Allow to disable qs footer warnings [2/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/QuickSettings.java
@@ -129,6 +129,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements Indexab
         LineageSettings.Secure.putIntForUser(resolver,
                 LineageSettings.Secure.QS_SHOW_AUTO_BRIGHTNESS, 1, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.QS_SHOW_SECURITY_FOOTER, 1, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.QUICK_SETTINGS_VIBRATE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.BLUETOOTH_QS_SHOW_BATTERY, 1, UserHandle.USER_CURRENT);

AlphaDroid-Project/packages_apps_AlphaSettings

src/com/alpha/settings/fragments/QuickSettings.java
@@ -77,6 +77,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_SHOW_BATTERY_PERCENT, 2, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_BATTERY_ESTIMATE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_WARNINGS, 1, UserHandle.USER_CURRENT);
     }
 
     @Override