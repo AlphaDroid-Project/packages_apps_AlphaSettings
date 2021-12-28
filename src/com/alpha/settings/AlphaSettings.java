02f82d28d44185457075a7abcb0f4eeb9cef5c79
Pranav Vashi <neobuddy89@gmail.com>
2021-12-28 13:09:46 +0530

    Add misc fragment
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/AlphaSettings.java
+++ src/com/alpha/settings/AlphaSettings.java
@@ -44,6 +44,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.alpha.settings.fragments.About;
 import com.alpha.settings.fragments.Buttons;
 import com.alpha.settings.fragments.LockScreen;
+import com.alpha.settings.fragments.Miscellaneous;
 import com.alpha.settings.fragments.Navigation;
 import com.alpha.settings.fragments.QuickSettings;
 import com.alpha.settings.fragments.Sound;
@@ -51,7 +52,6 @@ import com.alpha.settings.fragments.StatusBar;
 import com.alpha.settings.fragments.UserInterface;
 
 /*
-import com.alpha.settings.fragments.Miscellaneous;
 import com.alpha.settings.fragments.Notifications;
 */
 
@@ -135,11 +135,11 @@ public class AlphaSettings extends SettingsPreferenceFragment {
         @Override
         protected Void doInBackground(Void... params) {
 /*
-            Miscellaneous.reset(rContext);
             Notifications.reset(rContext);
 */
             Buttons.reset(rContext);
             LockScreen.reset(rContext);
+            Miscellaneous.reset(rContext);
             Navigation.reset(rContext);
             QuickSettings.reset(rContext);
             Sound.reset(rContext);
@@ -171,7 +171,6 @@ public class AlphaSettings extends SettingsPreferenceFragment {
             super(fm);
 /*
             frags[6] = new Notifications();
-            frags[8] = new Miscellaneous();
 */
             frags[0] = new StatusBar();
             frags[1] = new QuickSettings();
@@ -180,7 +179,8 @@ public class AlphaSettings extends SettingsPreferenceFragment {
             frags[4] = new Buttons();
             frags[5] = new UserInterface();
             frags[6] = new Sound();
-            frags[7] = new About();
+            frags[7] = new Miscellaneous();
+            frags[8] = new About();
         }
 
         @Override
@@ -204,7 +204,6 @@ public class AlphaSettings extends SettingsPreferenceFragment {
         titleString = new String[] {
 /*
             getString(R.string.notifications_title),
-            getString(R.string.misc_title),
 */
             getString(R.string.statusbar_title),
             getString(R.string.quicksettings_title),
@@ -213,6 +212,7 @@ public class AlphaSettings extends SettingsPreferenceFragment {
             getString(R.string.button_title),
             getString(R.string.ui_title),
             getString(R.string.sound_title),
+            getString(R.string.misc_title),
             getString(R.string.about_alpha)
         };
         return titleString;
