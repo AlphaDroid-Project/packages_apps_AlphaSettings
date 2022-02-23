e53cdbb25c0e3b1c9c133a82744dc445cef761e8
Pranav Vashi <neobuddy89@gmail.com>
2022-02-24 01:16:22 +0530

    Use separate packages for UDFPS icons and animations [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/lockscreen/udfps/UdfpsIconPicker.java
+++ src/com/alpha/settings/fragments/lockscreen/udfps/UdfpsIconPicker.java
@@ -70,7 +70,7 @@ public class UdfpsIconPicker extends SettingsPreferenceFragment {
 
     private Resources udfpsRes;
 
-    private String mPkg = "com.alpha.udfps.resources";
+    private String mPkg = "com.alpha.udfps.icons";
 
     private String[] mIcons;
 
