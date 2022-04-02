c737a9a052588fa09a6db0c636a714df6032a8b5
ShevT <evgeny.shishkov@gmail.com>
2022-04-03 01:21:32 +0300

    Clock: Add ability to resize QS & Statusbar Clock [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/statusbar/Clock.java
+++ src/com/alpha/settings/fragments/statusbar/Clock.java
@@ -228,6 +228,8 @@ public class Clock extends SettingsPreferenceFragment
         Settings.System.putIntForUser(resolver,
                 Settings.System.STATUSBAR_CLOCK_CHIP, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
+                Settings.System.STATUS_BAR_CLOCK_SIZE, 14, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
                 Settings.System.STATUS_BAR_AM_PM, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.STATUS_BAR_CLOCK_AUTO_HIDE, 0, UserHandle.USER_CURRENT);
