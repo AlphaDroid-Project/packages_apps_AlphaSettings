0a306f15664cfb872df549e5e389adbba7502d2b
Pranav Vashi <neobuddy89@gmail.com>
2022-10-01 12:48:58 +0530

    Add back navigation bar fragment
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/AlphaSettings.java
+++ src/com/alpha/settings/AlphaSettings.java
@@ -44,11 +44,11 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.alpha.settings.fragments.About;
 import com.alpha.settings.fragments.LockScreen;
 import com.alpha.settings.fragments.QuickSettings;
+import com.alpha.settings.fragments.Navigation;
 import com.alpha.settings.fragments.StatusBar;
 /*
 import com.alpha.settings.fragments.Buttons;
 import com.alpha.settings.fragments.Miscellaneous;
-import com.alpha.settings.fragments.Navigation;
 import com.alpha.settings.fragments.Notifications;
 import com.alpha.settings.fragments.Sound;
 import com.alpha.settings.fragments.UserInterface;
@@ -136,13 +136,13 @@ public class AlphaSettings extends SettingsPreferenceFragment {
 /*
             Buttons.reset(rContext);
             Miscellaneous.reset(rContext);
-            Navigation.reset(rContext);
             Notifications.reset(rContext);
             Sound.reset(rContext);
             UserInterface.reset(rContext);
 */
             LockScreen.reset(rContext);
             QuickSettings.reset(rContext);
+            Navigation.reset(rContext);
             StatusBar.reset(rContext);
             finish();
             startActivity(getIntent());
@@ -169,7 +169,6 @@ public class AlphaSettings extends SettingsPreferenceFragment {
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
 /*
-            frags[3] = new Navigation();
             frags[4] = new Buttons();
             frags[5] = new UserInterface();
             frags[6] = new Notifications();
@@ -179,7 +178,8 @@ public class AlphaSettings extends SettingsPreferenceFragment {
             frags[0] = new StatusBar();
             frags[1] = new QuickSettings();
             frags[2] = new LockScreen();
-            frags[3] = new About();
+            frags[3] = new Navigation();
+            frags[4] = new About();
         }
 
         @Override
@@ -202,7 +202,6 @@ public class AlphaSettings extends SettingsPreferenceFragment {
         String titleString[];
         titleString = new String[] {
 /*
-            getString(R.string.navigation_title),
             getString(R.string.button_title),
             getString(R.string.ui_title),
             getString(R.string.notifications_title),
@@ -212,6 +211,7 @@ public class AlphaSettings extends SettingsPreferenceFragment {
             getString(R.string.statusbar_title),
             getString(R.string.quicksettings_title),
             getString(R.string.lockscreen_title),
+            getString(R.string.navigation_title),
             getString(R.string.about_alpha)
         };
         return titleString;
