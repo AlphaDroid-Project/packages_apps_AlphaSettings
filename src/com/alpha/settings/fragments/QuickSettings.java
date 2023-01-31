01cec2bbe5bab11bb4c86294314d305002943ff5
elpaablo <donopwhile1@gmail.com>
2023-01-31 17:07:14 +0000

    Revert "crdroid: Introduce OOS style notification clear all button [2/2]"
    This reverts commit 690c379628b7bfa513c354df35b7a4016717ced2.

--- src/com/alpha/settings/fragments/QuickSettings.java
+++ src/com/alpha/settings/fragments/QuickSettings.java
@@ -207,8 +207,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.SECURE_LOCKSCREEN_QS_DISABLED, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.NOTIFICATION_MATERIAL_DISMISS, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TRANSPARENCY, 100, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_FOOTER_DATA_USAGE, 0, UserHandle.USER_CURRENT);
