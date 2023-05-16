7a2b398dc8d285b12548a81fb50bcb6ab74c9d19
Pranav Vashi <neobuddy89@gmail.com>
2023-05-16 21:30:23 +0530

    crdroid: Move lockscreen clock customization to lockscreen settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/lockscreen/LockClockFontsPicker.java
+++ src/com/alpha/settings/fragments/lockscreen/LockClockFontsPicker.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.alpha.settings.fragments.ui;
+package com.alpha.settings.fragments.lockscreen;
 
 import static android.os.UserHandle.USER_SYSTEM;
 
