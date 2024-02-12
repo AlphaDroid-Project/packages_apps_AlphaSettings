crdroidandroid/android_packages_apps_crDroidSettings

crdroid: Revive udfps animations settings
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/LockScreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2023 crDroid Android Project
+ * Copyright (C) 2016-2024 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,14 +55,14 @@ public class LockScreen extends SettingsPreferenceFragment
 
     private static final String LOCKSCREEN_INTERFACE_CATEGORY = "lockscreen_interface_category";
     private static final String LOCKSCREEN_GESTURES_CATEGORY = "lockscreen_gestures_category";
-    private static final String KEY_UDFPS_SETTINGS = "udfps_settings";
     private static final String KEY_FP_SUCCESS_VIBRATE = "fp_success_vibrate";
     private static final String KEY_FP_ERROR_VIBRATE = "fp_error_vibrate";
     private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
     private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+    private static final String KEY_UDFPS_ANIMATIONS = "udfps_recognizing_animation_preview";
     private static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
 
-    private Preference mUdfpsSettings;
+    private Preference mUdfpsAnimations;
     private Preference mFingerprintVib;
     private Preference mFingerprintVibErr;
     private Preference mRippleEffect;
@@ -77,26 +77,25 @@ public class LockScreen extends SettingsPreferenceFragment
 
         addPreferencesFromResource(R.xml.crdroid_settings_lockscreen);
 
-        PreferenceCategory interfaceCategory = (PreferenceCategory) findPreference(LOCKSCREEN_INTERFACE_CATEGORY);
         PreferenceCategory gestCategory = (PreferenceCategory) findPreference(LOCKSCREEN_GESTURES_CATEGORY);
 
         FingerprintManager mFingerprintManager = (FingerprintManager)
                 getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
-        mUdfpsSettings = (Preference) findPreference(KEY_UDFPS_SETTINGS);
+        mUdfpsAnimations = (Preference) findPreference(KEY_UDFPS_ANIMATIONS);
         mFingerprintVib = (Preference) findPreference(KEY_FP_SUCCESS_VIBRATE);
         mFingerprintVibErr = (Preference) findPreference(KEY_FP_ERROR_VIBRATE);
         mRippleEffect = (Preference) findPreference(KEY_RIPPLE_EFFECT);
         mScreenOffUdfps = (Preference) findPreference(SCREEN_OFF_UDFPS_ENABLED);
 
         if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()) {
-            interfaceCategory.removePreference(mUdfpsSettings);
+            gestCategory.removePreference(mUdfpsAnimations);
             gestCategory.removePreference(mFingerprintVib);
             gestCategory.removePreference(mFingerprintVibErr);
             gestCategory.removePreference(mRippleEffect);
             gestCategory.removePreference(mScreenOffUdfps);
         } else {
-            if (!Utils.isPackageInstalled(getContext(), "com.crdroid.udfps.icons")) {
-                interfaceCategory.removePreference(mUdfpsSettings);
+            if (!Utils.isPackageInstalled(getContext(), "com.crdroid.udfps.animations")) {
+                gestCategory.removePreference(mUdfpsAnimations);
             }
             Resources resources = getResources();
             boolean screenOffUdfpsAvailable = resources.getBoolean(
@@ -178,14 +177,14 @@ public class LockScreen extends SettingsPreferenceFragment
                     FingerprintManager mFingerprintManager = (FingerprintManager)
                             context.getSystemService(Context.FINGERPRINT_SERVICE);
                     if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()) {
-                        keys.add(KEY_UDFPS_SETTINGS);
+                        keys.add(KEY_UDFPS_ANIMATIONS);
                         keys.add(KEY_FP_SUCCESS_VIBRATE);
                         keys.add(KEY_FP_ERROR_VIBRATE);
                         keys.add(KEY_RIPPLE_EFFECT);
                         keys.add(SCREEN_OFF_UDFPS_ENABLED);
                     } else {
-                        if (!Utils.isPackageInstalled(context, "com.crdroid.udfps.icons")) {
-                            keys.add(KEY_UDFPS_SETTINGS);
+                        if (!Utils.isPackageInstalled(context, "com.crdroid.udfps.animations")) {
+                            keys.add(KEY_UDFPS_ANIMATIONS);
                         }
                         Resources resources = context.getResources();
                         boolean screenOffUdfpsAvailable = resources.getBoolean(

src/com/crdroid/settings/fragments/lockscreen/UdfpsAnimation.java
@@ -1,0 +1,258 @@
+/*
+ * Copyright (C) 2022-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.lockscreen;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsActivity;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.widget.SettingsMainSwitchBar;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UdfpsAnimation extends SettingsPreferenceFragment implements
+        OnMainSwitchChangeListener {
+
+    private Switch mSwitch;
+
+    private RecyclerView mRecyclerView;
+    private String mPkg = "com.crdroid.udfps.animations";
+    private AnimationDrawable animation;
+
+    private Resources udfpsRes;
+
+    private String[] mAnims;
+    private String[] mAnimPreviews;
+    private String[] mTitles;
+
+    private boolean mEnabled;
+    private UdfpsAnimAdapter mUdfpsAnimAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.udfps_recog_animation_effect_title);
+
+        loadResources();
+    }
+
+    private void loadResources() {
+        try {
+            PackageManager pm = getActivity().getPackageManager();
+            udfpsRes = pm.getResourcesForApplication(mPkg);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        mAnims = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_animation_styles",
+                "array", mPkg));
+        mAnimPreviews = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_animation_previews",
+                "array", mPkg));
+        mTitles = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_animation_titles",
+                "array", mPkg));
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.item_view, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mUdfpsAnimAdapter = new UdfpsAnimAdapter(getActivity());
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final SettingsActivity activity = (SettingsActivity) getActivity();
+        final SettingsMainSwitchBar switchBar = activity.getSwitchBar();
+        mSwitch = switchBar.getSwitch();
+        mEnabled = Settings.System.getInt(getActivity().getContentResolver(),
+                       Settings.System.UDFPS_ANIM, 0) == 1;
+        mSwitch.setChecked(mEnabled);
+        setEnabled(mEnabled);
+        switchBar.setTitle(getActivity().getString(R.string.enable));
+        switchBar.addOnSwitchChangeListener(this);
+        switchBar.show();
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.UDFPS_ANIM, isChecked ? 1 : 0);
+        mSwitch.setChecked(isChecked);
+        setEnabled(isChecked);
+    }
+
+    public void setEnabled(boolean enabled) {
+        if (enabled) {
+            mRecyclerView.setAdapter(mUdfpsAnimAdapter);
+        } else {
+            mRecyclerView.setAdapter(null);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public class UdfpsAnimAdapter extends RecyclerView.Adapter<UdfpsAnimAdapter.UdfpsAnimViewHolder> {
+        Context context;
+        String mSelectedAnim;
+        String mAppliedAnim;
+
+        public UdfpsAnimAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public UdfpsAnimViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_option, parent, false);
+            UdfpsAnimViewHolder vh = new UdfpsAnimViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(UdfpsAnimViewHolder holder, final int position) {
+            String animName = mAnims[position];
+
+            Glide.with(holder.image.getContext())
+                    .load("")
+                    .placeholder(getDrawable(holder.image.getContext(), mAnimPreviews[position]))
+                    .into(holder.image);
+
+            holder.name.setText(mTitles[position]);
+
+            if (position == Settings.System.getInt(context.getContentResolver(),
+                Settings.System.UDFPS_ANIM_STYLE, 0)) {
+                mAppliedAnim = animName;
+                if (mSelectedAnim == null) {
+                    mSelectedAnim = animName;
+                }
+            }
+
+            holder.itemView.setActivated(animName == mSelectedAnim);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateActivatedStatus(mSelectedAnim, false);
+                    updateActivatedStatus(animName, true);
+                    mSelectedAnim = animName;
+                    holder.image.setBackgroundDrawable(getDrawable(v.getContext(), mAnims[position]));
+                    animation = (AnimationDrawable) holder.image.getBackground();
+                    animation.setOneShot(true);
+                    animation.start();
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.UDFPS_ANIM_STYLE, position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAnims.length;
+        }
+
+        public class UdfpsAnimViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            ImageView image;
+            public UdfpsAnimViewHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.option_label);
+                image = (ImageView) itemView.findViewById(R.id.option_thumbnail);
+            }
+        }
+
+        private void updateActivatedStatus(String anim, boolean isActivated) {
+            int index = Arrays.asList(mAnims).indexOf(anim);
+            if (index < 0) {
+                return;
+            }
+            RecyclerView.ViewHolder holder = mRecyclerView.findViewHolderForAdapterPosition(index);
+            if (holder != null && holder.itemView != null) {
+                holder.itemView.setActivated(isActivated);
+            }
+        }
+    }
+
+    public Drawable getDrawable(Context context, String drawableName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            Resources res = pm.getResourcesForApplication(mPkg);
+            return res.getDrawable(res.getIdentifier(drawableName, "drawable", mPkg));
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

AlphaDroid-Project/packages_apps_AlphaSettings

src/com/alpha/settings/fragments/LockScreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2023 crDroid Android Project
+ * Copyright (C) 2016-2024 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,14 +58,14 @@ public class LockScreen extends SettingsPreferenceFragment
 
     private static final String LOCKSCREEN_INTERFACE_CATEGORY = "lockscreen_interface_category";
     private static final String LOCKSCREEN_GESTURES_CATEGORY = "lockscreen_gestures_category";
-    private static final String KEY_UDFPS_SETTINGS = "udfps_settings";
     private static final String KEY_FP_SUCCESS_VIBRATE = "fp_success_vibrate";
     private static final String KEY_FP_ERROR_VIBRATE = "fp_error_vibrate";
     private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
     private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+    private static final String KEY_UDFPS_ANIMATIONS = "udfps_recognizing_animation_preview";
     private static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
 
-    private Preference mUdfpsSettings;
+    private Preference mUdfpsAnimations;
     private Preference mFingerprintVib;
     private Preference mFingerprintVibErr;
     private Preference mRippleEffect;
@@ -80,27 +80,26 @@ public class LockScreen extends SettingsPreferenceFragment
 
         addPreferencesFromResource(R.xml.alpha_settings_lockscreen);
 
-        PreferenceCategory interfaceCategory = (PreferenceCategory) findPreference(LOCKSCREEN_INTERFACE_CATEGORY);
         PreferenceCategory gestCategory = (PreferenceCategory) findPreference(LOCKSCREEN_GESTURES_CATEGORY);
 
         FingerprintManager mFingerprintManager = (FingerprintManager)
                 getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
-        mUdfpsSettings = (Preference) findPreference(KEY_UDFPS_SETTINGS);
+        mUdfpsAnimations = (Preference) findPreference(KEY_UDFPS_ANIMATIONS);
         mFingerprintVib = (Preference) findPreference(KEY_FP_SUCCESS_VIBRATE);
         mFingerprintVibErr = (Preference) findPreference(KEY_FP_ERROR_VIBRATE);
         mRippleEffect = (Preference) findPreference(KEY_RIPPLE_EFFECT);
         mScreenOffUdfps = (Preference) findPreference(SCREEN_OFF_UDFPS_ENABLED);
 
         if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()) {
-            interfaceCategory.removePreference(mUdfpsSettings);
+            gestCategory.removePreference(mUdfpsAnimations);
             gestCategory.removePreference(mFingerprintVib);
             gestCategory.removePreference(mFingerprintVibErr);
             gestCategory.removePreference(mRippleEffect);
             gestCategory.removePreference(mScreenOffUdfps);
         } else {
-            //if (!Utils.isPackageInstalled(getContext(), "com.alpha.udfps.icons")) {
-                interfaceCategory.removePreference(mUdfpsSettings);
-            //}
+            if (!Utils.isPackageInstalled(getContext(), "com.alpha.udfps.animations")) {
+                gestCategory.removePreference(mUdfpsAnimations);
+            }
             Resources resources = getResources();
             boolean screenOffUdfpsAvailable = resources.getBoolean(
                     com.android.internal.R.bool.config_supportScreenOffUdfps) ||
@@ -183,15 +182,15 @@ public class LockScreen extends SettingsPreferenceFragment
                     FingerprintManager mFingerprintManager = (FingerprintManager)
                             context.getSystemService(Context.FINGERPRINT_SERVICE);
                     if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()) {
-                        keys.add(KEY_UDFPS_SETTINGS);
+                        keys.add(KEY_UDFPS_ANIMATIONS);
                         keys.add(KEY_FP_SUCCESS_VIBRATE);
                         keys.add(KEY_FP_ERROR_VIBRATE);
                         keys.add(KEY_RIPPLE_EFFECT);
                         keys.add(SCREEN_OFF_UDFPS_ENABLED);
                     } else {
-                        //if (!Utils.isPackageInstalled(context, "com.alpha.udfps.icons")) {
-                            keys.add(KEY_UDFPS_SETTINGS);
-                        //}
+                        if (!Utils.isPackageInstalled(context, "com.alpha.udfps.animations")) {
+                            keys.add(KEY_UDFPS_ANIMATIONS);
+                        }
                         Resources resources = context.getResources();
                         boolean screenOffUdfpsAvailable = resources.getBoolean(
                             com.android.internal.R.bool.config_supportScreenOffUdfps) ||

src/com/alpha/settings/fragments/lockscreen/UdfpsAnimation.java
@@ -1,0 +1,258 @@
+/*
+ * Copyright (C) 2022-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alpha.settings.fragments.lockscreen;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsActivity;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.widget.SettingsMainSwitchBar;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UdfpsAnimation extends SettingsPreferenceFragment implements
+        OnMainSwitchChangeListener {
+
+    private Switch mSwitch;
+
+    private RecyclerView mRecyclerView;
+    private String mPkg = "com.alpha.udfps.animations";
+    private AnimationDrawable animation;
+
+    private Resources udfpsRes;
+
+    private String[] mAnims;
+    private String[] mAnimPreviews;
+    private String[] mTitles;
+
+    private boolean mEnabled;
+    private UdfpsAnimAdapter mUdfpsAnimAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.udfps_recog_animation_effect_title);
+
+        loadResources();
+    }
+
+    private void loadResources() {
+        try {
+            PackageManager pm = getActivity().getPackageManager();
+            udfpsRes = pm.getResourcesForApplication(mPkg);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        mAnims = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_animation_styles",
+                "array", mPkg));
+        mAnimPreviews = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_animation_previews",
+                "array", mPkg));
+        mTitles = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_animation_titles",
+                "array", mPkg));
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.item_view, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mUdfpsAnimAdapter = new UdfpsAnimAdapter(getActivity());
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final SettingsActivity activity = (SettingsActivity) getActivity();
+        final SettingsMainSwitchBar switchBar = activity.getSwitchBar();
+        mSwitch = switchBar.getSwitch();
+        mEnabled = Settings.System.getInt(getActivity().getContentResolver(),
+                       Settings.System.UDFPS_ANIM, 0) == 1;
+        mSwitch.setChecked(mEnabled);
+        setEnabled(mEnabled);
+        switchBar.setTitle(getActivity().getString(R.string.enable));
+        switchBar.addOnSwitchChangeListener(this);
+        switchBar.show();
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.UDFPS_ANIM, isChecked ? 1 : 0);
+        mSwitch.setChecked(isChecked);
+        setEnabled(isChecked);
+    }
+
+    public void setEnabled(boolean enabled) {
+        if (enabled) {
+            mRecyclerView.setAdapter(mUdfpsAnimAdapter);
+        } else {
+            mRecyclerView.setAdapter(null);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.ALPHA;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public class UdfpsAnimAdapter extends RecyclerView.Adapter<UdfpsAnimAdapter.UdfpsAnimViewHolder> {
+        Context context;
+        String mSelectedAnim;
+        String mAppliedAnim;
+
+        public UdfpsAnimAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public UdfpsAnimViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_option, parent, false);
+            UdfpsAnimViewHolder vh = new UdfpsAnimViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(UdfpsAnimViewHolder holder, final int position) {
+            String animName = mAnims[position];
+
+            Glide.with(holder.image.getContext())
+                    .load("")
+                    .placeholder(getDrawable(holder.image.getContext(), mAnimPreviews[position]))
+                    .into(holder.image);
+
+            holder.name.setText(mTitles[position]);
+
+            if (position == Settings.System.getInt(context.getContentResolver(),
+                Settings.System.UDFPS_ANIM_STYLE, 0)) {
+                mAppliedAnim = animName;
+                if (mSelectedAnim == null) {
+                    mSelectedAnim = animName;
+                }
+            }
+
+            holder.itemView.setActivated(animName == mSelectedAnim);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateActivatedStatus(mSelectedAnim, false);
+                    updateActivatedStatus(animName, true);
+                    mSelectedAnim = animName;
+                    holder.image.setBackgroundDrawable(getDrawable(v.getContext(), mAnims[position]));
+                    animation = (AnimationDrawable) holder.image.getBackground();
+                    animation.setOneShot(true);
+                    animation.start();
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.UDFPS_ANIM_STYLE, position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAnims.length;
+        }
+
+        public class UdfpsAnimViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            ImageView image;
+            public UdfpsAnimViewHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.option_label);
+                image = (ImageView) itemView.findViewById(R.id.option_thumbnail);
+            }
+        }
+
+        private void updateActivatedStatus(String anim, boolean isActivated) {
+            int index = Arrays.asList(mAnims).indexOf(anim);
+            if (index < 0) {
+                return;
+            }
+            RecyclerView.ViewHolder holder = mRecyclerView.findViewHolderForAdapterPosition(index);
+            if (holder != null && holder.itemView != null) {
+                holder.itemView.setActivated(isActivated);
+            }
+        }
+    }
+
+    public Drawable getDrawable(Context context, String drawableName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            Resources res = pm.getResourcesForApplication(mPkg);
+            return res.getDrawable(res.getIdentifier(drawableName, "drawable", mPkg));
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}