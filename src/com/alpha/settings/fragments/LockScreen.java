crdroidandroid/android_packages_apps_crDroidSettings

crdroid: Add reset settings for udfps anim
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/LockScreen.java
@@ -43,6 +43,8 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.crdroid.settings.fragments.lockscreen.UdfpsAnimation;
+
 import java.util.List;
 
 import lineageos.providers.LineageSettings;
@@ -142,6 +144,7 @@ public class LockScreen extends SettingsPreferenceFragment
                 Settings.System.LOCKSCREEN_WEATHER_LOCATION, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LOCKSCREEN_WEATHER_TEXT, 1, UserHandle.USER_CURRENT);
+        UdfpsAnimation.reset(mContext);
     }
 
     private void updateWeatherSettings() {

AlphaDroid-Project/packages_apps_AlphaSettings

src/com/alpha/settings/fragments/LockScreen.java
@@ -46,6 +46,8 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.alpha.settings.fragments.lockscreen.UdfpsAnimation;
+
 import java.util.List;
 
 import lineageos.providers.LineageSettings;
@@ -145,8 +147,8 @@ public class LockScreen extends SettingsPreferenceFragment
                 Settings.System.LOCKSCREEN_WEATHER_LOCATION, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LOCKSCREEN_WEATHER_TEXT, 1, UserHandle.USER_CURRENT);
-        UdfpsSettings.reset(mContext);
         DozeSettings.reset(mContext);
+        UdfpsAnimation.reset(mContext);
     }
 
     private void updateWeatherSettings() {