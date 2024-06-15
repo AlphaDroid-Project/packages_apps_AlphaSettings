xiaomi-sm8450-marble/android_packages_apps_Personalizations

QuickSettings: Media player mode toggle [2/2]

src/com/crdroid/settings/fragments/QuickSettings.java
@@ -42,6 +42,8 @@ import com.crdroid.settings.preferences.CustomSeekBarPreference;
 
 import lineageos.providers.LineageSettings;
 
+import com.android.internal.util.rising.SystemRestartUtils;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -59,6 +61,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
     private static final String KEY_QS_UI_STYLE  = "qs_tile_ui_style";
     private static final String KEY_QS_PANEL_STYLE  = "qs_panel_style";
+    private static final String KEY_QS_COMPACT_PLAYER  = "qs_compact_media_player_mode";
 
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
@@ -68,6 +71,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private ListPreference mTileAnimationInterpolator;
     private ListPreference mQsUI;
     private ListPreference mQsPanelStyle;
+    private Preference mQsCompactPlayer;
 
     private static ThemeUtils mThemeUtils;
 
@@ -115,6 +119,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mQsPanelStyle = (ListPreference) findPreference(KEY_QS_PANEL_STYLE);
         mQsPanelStyle.setOnPreferenceChangeListener(this);
+        
+        mQsCompactPlayer = (Preference) findPreference(KEY_QS_COMPACT_PLAYER);
+        mQsCompactPlayer.setOnPreferenceChangeListener(this);
 
         checkQSOverlays(mContext);
     }
@@ -146,6 +153,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                     Settings.System.QS_PANEL_STYLE, value, UserHandle.USER_CURRENT);
             updateQsPanelStyle(getActivity());
             checkQSOverlays(getActivity());
+            return true;
+        } else if (preference == mQsCompactPlayer) {
+            SystemRestartUtils.showSystemUIRestartDialog(getActivity());
             return true;
         }
         return false;

AlphaDroid-Project/packages_apps_AlphaSettings

src/com/alpha/settings/fragments/QuickSettings.java
@@ -41,6 +41,8 @@ import com.alpha.settings.preferences.CustomSeekBarPreference;
 
 import lineageos.providers.LineageSettings;
 
+import com.android.internal.util.alpha.SystemRestartUtils;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -56,6 +58,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String KEY_PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
     private static final String KEY_PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
+    private static final String KEY_QS_COMPACT_PLAYER  = "qs_compact_media_player_mode";
 
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
@@ -63,6 +66,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private ListPreference mTileAnimationStyle;
     private CustomSeekBarPreference mTileAnimationDuration;
     private ListPreference mTileAnimationInterpolator;
+    private Preference mQsCompactPlayer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -99,6 +103,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         int tileAnimationStyle = Settings.System.getIntForUser(resolver,
                 Settings.System.QS_TILE_ANIMATION_STYLE, 0, UserHandle.USER_CURRENT);
         updateAnimTileStyle(tileAnimationStyle);
+        
+        mQsCompactPlayer = (Preference) findPreference(KEY_QS_COMPACT_PLAYER);
+        mQsCompactPlayer.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -114,6 +122,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         } else if (preference == mTileAnimationStyle) {
             int value = Integer.parseInt((String) newValue);
             updateAnimTileStyle(value);
+            return true;
+        } else if (preference == mQsCompactPlayer) {
+            SystemRestartUtils.showSystemUIRestartDialog(getActivity());
             return true;
         }
         return false;