c29b8de42dce8307a7b74c0eed41a32ce5492042
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 14:34:37 +0530

    Switch to json-based (PIF) spoofing [2/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: elpaablo <donopwhile1@gmail.com>

--- src/com/alpha/settings/fragments/misc/Spoofing.java
+++ src/com/alpha/settings/fragments/misc/Spoofing.java
@@ -15,18 +15,34 @@
  */
 package com.alpha.settings.fragments.misc;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.util.Log;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto;
-
+import com.android.internal.util.alpha.SystemRestartUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.JSONObject;
+
 
 public class Spoofing extends SettingsPreferenceFragment {
 
@@ -38,10 +54,55 @@ public class Spoofing extends SettingsPreferenceFragment {
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_SNAPCHAT_SPOOF = "persist.sys.pixelprops.snapchat";
 
+    private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
+
+    private Preference mPifJsonFilePreference;
+    private Handler mHandler;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.spoofing_settings);
+        mHandler = new Handler();
+        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mPifJsonFilePreference) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("application/json");
+            startActivityForResult(intent, 10001);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10001 && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            Log.d(TAG, "URI received: " + uri.toString());
+            try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+                if (inputStream != null) {
+                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                    Log.d(TAG, "JSON data: " + json);
+                    JSONObject jsonObject = new JSONObject(json);
+                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                        String key = it.next();
+                        String value = jsonObject.getString(key);
+                        Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
+                        SystemProperties.set("persist.sys.pihooks_" + key, value);
+                    }
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error reading JSON or setting properties", e);
+            }
+            mHandler.postDelayed(() -> {
+                SystemRestartUtils.showSystemRestartDialog(getContext());
+            }, 1250);
+        }
     }
 
     public static void reset(Context mContext) {
