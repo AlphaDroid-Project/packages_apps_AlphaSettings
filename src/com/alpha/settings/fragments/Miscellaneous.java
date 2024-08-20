4d365fc7879acc3e62747bcb5ec3a7c611dd5c69
Pranav Vashi <neobuddy89@gmail.com>
2024-08-21 00:10:32 +0530

    crdroid: Remove three finger swipe to screenshot
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/Miscellaneous.java
+++ src/com/alpha/settings/fragments/Miscellaneous.java
@@ -77,8 +77,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.POCKET_JUDGE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
                 Settings.System.AUTO_BRIGHTNESS_ONE_SHOT, 0, UserHandle.USER_CURRENT);
         SystemProperties.set(SYS_GAMES_SPOOF, "false");
         SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
