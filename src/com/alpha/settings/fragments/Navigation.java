crdroidandroid/android_packages_apps_crDroidSettings

crDroidSettings: Allow to customize bottom corner swipe up action [4/4]
Change-Id: I7ddf44a8bff04f3c4b5b08e9e0491a7b8e10a464

src/com/crdroid/settings/fragments/Navigation.java
@@ -56,6 +56,7 @@ public class Navigation extends SettingsPreferenceFragment implements
     private static final String KEY_NAVIGATION_HOME_DOUBLE_TAP = "navigation_home_double_tap";
     private static final String KEY_NAVIGATION_APP_SWITCH_LONG_PRESS =
             "navigation_app_switch_long_press";
+    private static final String KEY_CORNER_LONG_SWIPE = "navigation_bar_corner_long_swipe";
     private static final String KEY_EDGE_LONG_SWIPE = "navigation_bar_edge_long_swipe";
 
     private SwitchPreferenceCompat mNavbarVisibility;
@@ -63,6 +64,7 @@ public class Navigation extends SettingsPreferenceFragment implements
     private ListPreference mNavigationHomeLongPressAction;
     private ListPreference mNavigationHomeDoubleTapAction;
     private ListPreference mNavigationAppSwitchLongPressAction;
+    private ListPreference mCornerLongSwipeAction;
     private ListPreference mEdgeLongSwipeAction;
 
     private boolean mIsNavSwitchingMode = false;
@@ -107,6 +109,9 @@ public class Navigation extends SettingsPreferenceFragment implements
         Action appSwitchLongPressAction = Action.fromSettings(resolver,
                 LineageSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
                 defaultAppSwitchLongPressAction);
+        Action cornerLongSwipeAction = Action.fromSettings(resolver,
+                LineageSettings.System.KEY_CORNER_LONG_SWIPE_ACTION,
+                Action.SEARCH);
         Action edgeLongSwipeAction = Action.fromSettings(resolver,
                 LineageSettings.System.KEY_EDGE_LONG_SWIPE_ACTION,
                 Action.NOTHING);
@@ -126,6 +131,9 @@ public class Navigation extends SettingsPreferenceFragment implements
         // Navigation bar app switch long press
         mNavigationAppSwitchLongPressAction = initList(KEY_NAVIGATION_APP_SWITCH_LONG_PRESS,
                 appSwitchLongPressAction);
+
+        // Corner swipe up gesture
+        mCornerLongSwipeAction = initList(KEY_CORNER_LONG_SWIPE, cornerLongSwipeAction);
 
         // Edge long swipe gesture
         mEdgeLongSwipeAction = initList(KEY_EDGE_LONG_SWIPE, edgeLongSwipeAction);
@@ -187,6 +195,10 @@ public class Navigation extends SettingsPreferenceFragment implements
         } else if (preference == mNavigationAppSwitchLongPressAction) {
             handleListChange((ListPreference) preference, newValue,
                     LineageSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mCornerLongSwipeAction) {
+            handleListChange((ListPreference) preference, newValue,
+                    LineageSettings.System.KEY_CORNER_LONG_SWIPE_ACTION);
             return true;
         } else if (preference == mEdgeLongSwipeAction) {
             handleListChange((ListPreference) preference, newValue,

AlphaDroid-Project/packages_apps_AlphaSettings

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/alpha/settings/fragments/Navigation.java
@@ -56,6 +56,7 @@ public class Navigation extends SettingsPreferenceFragment implements
     private static final String KEY_NAVIGATION_HOME_DOUBLE_TAP = "navigation_home_double_tap";
     private static final String KEY_NAVIGATION_APP_SWITCH_LONG_PRESS =
             "navigation_app_switch_long_press";
+    private static final String KEY_CORNER_LONG_SWIPE = "navigation_bar_corner_long_swipe";
     private static final String KEY_EDGE_LONG_SWIPE = "navigation_bar_edge_long_swipe";
 
     private SwitchPreferenceCompat mNavbarVisibility;
@@ -63,6 +64,7 @@ public class Navigation extends SettingsPreferenceFragment implements
     private ListPreference mNavigationHomeLongPressAction;
     private ListPreference mNavigationHomeDoubleTapAction;
     private ListPreference mNavigationAppSwitchLongPressAction;
+    private ListPreference mCornerLongSwipeAction;
     private ListPreference mEdgeLongSwipeAction;
 
     private boolean mIsNavSwitchingMode = false;
@@ -107,6 +109,9 @@ public class Navigation extends SettingsPreferenceFragment implements
         Action appSwitchLongPressAction = Action.fromSettings(resolver,
                 Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
                 defaultAppSwitchLongPressAction);
+        Action cornerLongSwipeAction = Action.fromSettings(resolver,
+                Settings.System.KEY_CORNER_LONG_SWIPE_ACTION,
+                Action.SEARCH);
         Action edgeLongSwipeAction = Action.fromSettings(resolver,
                 Settings.System.KEY_EDGE_LONG_SWIPE_ACTION,
                 Action.NOTHING);
@@ -126,6 +131,9 @@ public class Navigation extends SettingsPreferenceFragment implements
         // Navigation bar app switch long press
         mNavigationAppSwitchLongPressAction = initList(KEY_NAVIGATION_APP_SWITCH_LONG_PRESS,
                 appSwitchLongPressAction);
+
+        // Corner swipe up gesture
+        mCornerLongSwipeAction = initList(KEY_CORNER_LONG_SWIPE, cornerLongSwipeAction);
 
         // Edge long swipe gesture
         mEdgeLongSwipeAction = initList(KEY_EDGE_LONG_SWIPE, edgeLongSwipeAction);
@@ -187,6 +195,10 @@ public class Navigation extends SettingsPreferenceFragment implements
         } else if (preference == mNavigationAppSwitchLongPressAction) {
             handleListChange((ListPreference) preference, newValue,
                     Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mCornerLongSwipeAction) {
+            handleListChange((ListPreference) preference, newValue,
+                    Settings.System.KEY_CORNER_LONG_SWIPE_ACTION);
             return true;
         } else if (preference == mEdgeLongSwipeAction) {
             handleListChange((ListPreference) preference, newValue,