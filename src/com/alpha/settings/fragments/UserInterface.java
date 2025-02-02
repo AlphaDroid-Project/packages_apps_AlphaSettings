f2cfed64bc7d26152cddda74dd60169270a2dfb6
minaripenguin <minaripenguin@users.noreply.github.com>
2025-02-03 05:37:15 +0900

    Add notification and powermenu styles
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/UserInterface.java
+++ src/com/alpha/settings/fragments/UserInterface.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2024 crDroid Android Project
+ * Copyright (C) 2016-2025 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
@@ -40,18 +41,42 @@ import com.alpha.settings.fragments.lockscreen.DozeSettings;
 import com.alpha.settings.fragments.misc.SmartPixels;
 import com.alpha.settings.fragments.ui.MonetSettings;
 
+import com.android.internal.util.alpha.ThemeUtils;
+
 import java.util.List;
 
 @SearchIndexable
-public class UserInterface extends SettingsPreferenceFragment {
+public class UserInterface extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "UserInterface";
 
     private static final String KEY_FORCE_FULL_SCREEN = "display_cutout_force_fullscreen_settings";
     private static final String SMART_PIXELS = "smart_pixels";
+    private static final String KEY_NOTIFICATION_STYLE = "notification_style";
+    private static final String KEY_POWERMENU_STYLE = "powermenu_style";
+
+    private static final String[] NOTIF_OVERLAYS = {
+            "com.android.theme.notification.cyberpunk",
+            "com.android.theme.notification.duoline",
+            "com.android.theme.notification.fluid",
+            "com.android.theme.notification.ios",
+            "com.android.theme.notification.layers"
+    };
+
+    private static final String[] POWERMENU_OVERLAYS = {
+            "com.android.theme.powermenu.cyberpunk",
+            "com.android.theme.powermenu.duoline",
+            "com.android.theme.powermenu.fluid",
+            "com.android.theme.powermenu.ios",
+            "com.android.theme.powermenu.layers"
+    };
 
     private Preference mShowCutoutForce;
     private Preference mSmartPixels;
+    private ListPreference mNotificationStylePref;
+    private ListPreference mPowermenuStylePref;
+    private ThemeUtils mThemeUtils;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,6 +100,42 @@ public class UserInterface extends SettingsPreferenceFragment {
                 com.android.internal.R.bool.config_supportSmartPixels);
         if (!mSmartPixelsSupported)
             prefScreen.removePreference(mSmartPixels);
+
+        mThemeUtils = new ThemeUtils(getContext());
+        mNotificationStylePref = findPreference(KEY_NOTIFICATION_STYLE);
+        mNotificationStylePref.setOnPreferenceChangeListener(this);
+        mPowermenuStylePref = findPreference(KEY_POWERMENU_STYLE);
+        mPowermenuStylePref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mNotificationStylePref) {
+            int value = Integer.parseInt((String) newValue);
+            updateNotifStyle(value);
+            return true;
+        } else if (preference == mPowermenuStylePref) {
+            int value = Integer.parseInt((String) newValue);
+            updatePowermenuStyle(value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateNotifStyle(int style) {
+        updateStyle(KEY_NOTIFICATION_STYLE, "android.theme.customization.notification", "com.android.systemui", 0, NOTIF_OVERLAYS, style);
+    }
+
+    private void updatePowermenuStyle(int style) {
+        updateStyle(KEY_POWERMENU_STYLE, "android.theme.customization.powermenu", "com.android.systemui", 0, POWERMENU_OVERLAYS, style);
+    }
+
+    private void updateStyle(String key, String category, String target,
+            int defaultValue, String[] overlayPackages, int style) {
+        mThemeUtils.setOverlayEnabled(category, target, target);
+        if (style > 0 && style <= overlayPackages.length) {
+            mThemeUtils.setOverlayEnabled(category, overlayPackages[style - 1], target);
+        }
     }
 
     public static void reset(Context mContext) {
