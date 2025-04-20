4532f11b826a452db92c10990c8a6b963852adc5
Ghosuto <clash.raja10@gmail.com>
2025-04-20 04:48:23 +0000

    Add ongoing action chip implementation
    Co-authored-by: Dmitrii <bankersenator@gmail.com>
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/StatusBar.java
+++ src/com/alpha/settings/fragments/StatusBar.java
@@ -42,6 +42,7 @@ import com.android.settingslib.search.SearchIndexable;
 import com.alpha.settings.fragments.statusbar.BatteryBar;
 import com.alpha.settings.fragments.statusbar.Clock;
 import com.alpha.settings.fragments.statusbar.NetworkTrafficSettings;
+import com.alpha.settings.fragments.statusbar.OngoingProgressBar;
 import com.alpha.settings.preferences.SystemSettingSeekBarPreference;
 import com.alpha.settings.utils.DeviceUtils;
 
@@ -162,6 +163,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
         BatteryBar.reset(mContext);
         Clock.reset(mContext);
         NetworkTrafficSettings.reset(mContext);
+        OngoingProgressBar.reset(mContext);
     }
 
     private void updateQuickPulldownSummary(int value) {
