3a1e3786525a70dd78c1836302703aa011a8b71f
Pranav Vashi <neobuddy89@gmail.com>
2025-09-14 22:03:55 +0530

    Add toggle for QS tile haptic feedback
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/alpha/settings/fragments/QuickSettings.java
+++ src/com/alpha/settings/fragments/QuickSettings.java
@@ -122,6 +122,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_DATA_USAGE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
+                Settings.System.QS_TILE_HAPTIC, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TILE_SHAPE, 0, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT);
