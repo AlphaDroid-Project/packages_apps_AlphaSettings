9776b841336b349a7c25f38dfdba262966d3ca2f
elpaablo <donopwhile1@gmail.com>
2026-03-22 01:20:34 +0000

    Cutout progress cleanup
    * Let's keep it simple

--- src/com/alpha/settings/fragments/statusbar/CutoutProgressSettingsFragment.kt
+++ src/com/alpha/settings/fragments/statusbar/CutoutProgressSettingsFragment.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2024-2026 Lunaris AOSP
+ * Copyright (C) 2026 AlphaDroid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,168 +26,64 @@ import com.android.settings.R
 import com.android.settings.SettingsPreferenceFragment
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent
 import com.alpha.settings.utils.toArgb
-import com.alpha.settings.utils.toHexString
 
 class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
     Preference.OnPreferenceChangeListener {
 
     companion object {
         private const val KEY_RING_COLOR_MODE = "cutout_progress_ring_color_mode"
-        private const val COLOR_MODE_ACCENT = 0
-        private const val COLOR_MODE_RAINBOW = 1
         private const val COLOR_MODE_CUSTOM = 2
 
         private const val KEY_RING_COLOR = "cutout_progress_ring_color"
-        private const val KEY_ERROR_COLOR = "cutout_progress_error_color"
-        private const val KEY_FLASH_COLOR = "cutout_progress_finish_flash_color"
-        private const val KEY_BG_COLOR = "cutout_progress_bg_ring_color"
-        private const val KEY_FINISH_STYLE = "cutout_progress_finish_style"
-        private const val KEY_EASING = "cutout_progress_easing"
-        private const val KEY_PERCENT_POSITION = "cutout_progress_percent_position"
-        private const val KEY_FILENAME_POSITION = "cutout_progress_filename_position"
-        private const val KEY_FILENAME_TRUNCATE = "cutout_progress_filename_truncate"
-
         private const val DEFAULT_RING_COLOR = 0xFF2196F3.toInt()
-        private const val DEFAULT_ERROR_COLOR = 0xFFF44336.toInt()
-        private const val DEFAULT_FLASH_COLOR = 0xFFFFFFFF.toInt()
-        private const val DEFAULT_BG_COLOR = 0xFF808080.toInt()
     }
 
     private lateinit var ringColorModePref: ListPreference
-
     private lateinit var ringColorPref: Preference
-    private lateinit var errorColorPref: Preference
-    private lateinit var flashColorPref: Preference
-    private lateinit var bgColorPref: Preference
-
-    private lateinit var finishStylePref: ListPreference
-    private lateinit var easingPref: ListPreference
-    private lateinit var pctPosPref: ListPreference
-    private lateinit var fnamePosPref: ListPreference
-    private lateinit var fnameTruncPref: ListPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.cutout_progress_settings)
 
         ringColorModePref = findPreference(KEY_RING_COLOR_MODE)!!
-
         ringColorPref = findPreference(KEY_RING_COLOR)!!
-        errorColorPref = findPreference(KEY_ERROR_COLOR)!!
-        flashColorPref = findPreference(KEY_FLASH_COLOR)!!
-        bgColorPref = findPreference(KEY_BG_COLOR)!!
 
-        finishStylePref = findPreference(KEY_FINISH_STYLE)!!
-        easingPref = findPreference(KEY_EASING)!!
-        pctPosPref = findPreference(KEY_PERCENT_POSITION)!!
-        fnamePosPref = findPreference(KEY_FILENAME_POSITION)!!
-        fnameTruncPref = findPreference(KEY_FILENAME_TRUNCATE)!!
-
-        refreshColorSummaries()
-        syncListPreferences()
-
-        val storedMode = readSecureInt(KEY_RING_COLOR_MODE, COLOR_MODE_ACCENT)
+        val storedMode = readSystemInt(KEY_RING_COLOR_MODE, 0)
         ringColorModePref.value = storedMode.toString()
-        updateColorPickerVisibility(storedMode)
-
+        ringColorPref.isVisible = (storedMode == COLOR_MODE_CUSTOM)
         ringColorModePref.onPreferenceChangeListener = this
 
+        ringColorPref.summary = "#${argbToHex(readSystemInt(KEY_RING_COLOR, DEFAULT_RING_COLOR))}"
         ringColorPref.setOnPreferenceClickListener {
-            showColorPicker(
+            val currentHex = argbToHex(readSystemInt(KEY_RING_COLOR, DEFAULT_RING_COLOR))
+            val dialog = CutoutProgressColorPickerDialogFragment.newInstance(
                 title = getString(R.string.cutout_progress_ring_color_title),
-                key = KEY_RING_COLOR,
-                default = DEFAULT_RING_COLOR
+                colorHex = currentHex
             )
+            dialog.setOnColorSelectedListener { color: Color ->
+                writeSystemInt(KEY_RING_COLOR, color.toArgb())
+                ringColorPref.summary = "#${argbToHex(color.toArgb())}"
+            }
+            dialog.show(parentFragmentManager, CutoutProgressColorPickerDialogFragment.TAG)
             true
         }
-        errorColorPref.setOnPreferenceClickListener {
-            showColorPicker(
-                title = getString(R.string.cutout_progress_error_color_title),
-                key = KEY_ERROR_COLOR,
-                default = DEFAULT_ERROR_COLOR
-            )
-            true
-        }
-        flashColorPref.setOnPreferenceClickListener {
-            showColorPicker(
-                title = getString(R.string.cutout_progress_finish_flash_color_title),
-                key = KEY_FLASH_COLOR,
-                default = DEFAULT_FLASH_COLOR
-            )
-            true
-        }
-        bgColorPref.setOnPreferenceClickListener {
-            showColorPicker(
-                title = getString(R.string.cutout_progress_bg_ring_color_title),
-                key = KEY_BG_COLOR,
-                default = DEFAULT_BG_COLOR
-            )
-            true
-        }
-
-        finishStylePref.onPreferenceChangeListener = this
-        easingPref.onPreferenceChangeListener = this
-        pctPosPref.onPreferenceChangeListener = this
-        fnamePosPref.onPreferenceChangeListener = this
-        fnameTruncPref.onPreferenceChangeListener = this
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
         val intValue = (newValue as? String)?.toIntOrNull() ?: return false
 
         if (preference.key == KEY_RING_COLOR_MODE) {
-            writeSecureInt(KEY_RING_COLOR_MODE, intValue)
-            updateColorPickerVisibility(intValue)
-            return true
+            ringColorPref.isVisible = (intValue == COLOR_MODE_CUSTOM)
         }
 
-        writeSecureInt(preference.key, intValue)
+        writeSystemInt(preference.key, intValue)
         return true
     }
 
-    private fun updateColorPickerVisibility(mode: Int) {
-        val isCustom = mode == COLOR_MODE_CUSTOM
-        ringColorPref.isVisible = isCustom
-    }
+    private fun readSystemInt(key: String, default: Int): Int =
+        Settings.System.getInt(requireContext().contentResolver, key, default)
 
-    private fun syncListPreferences() {
-        listOf(
-            finishStylePref to 0,
-            easingPref to 0,
-            pctPosPref to 0,
-            fnamePosPref to 4,
-            fnameTruncPref to 0
-        ).forEach { (pref, default) ->
-            pref.value = readSecureInt(pref.key, default).toString()
-        }
-    }
-
-    private fun showColorPicker(title: String, key: String, default: Int) {
-        val currentArgb = readSecureInt(key, default)
-        val currentHex = argbToHex(currentArgb)
-
-        val dialog = CutoutProgressColorPickerDialogFragment.newInstance(
-            title = title,
-            colorHex = currentHex
-        )
-        dialog.setOnColorSelectedListener { color: Color ->
-            writeSecureInt(key, color.toArgb())
-            refreshColorSummaries()
-        }
-        dialog.show(parentFragmentManager, CutoutProgressColorPickerDialogFragment.TAG)
-    }
-
-    private fun refreshColorSummaries() {
-        ringColorPref.summary = "#${argbToHex(readSecureInt(KEY_RING_COLOR, DEFAULT_RING_COLOR))}"
-        errorColorPref.summary = "#${argbToHex(readSecureInt(KEY_ERROR_COLOR, DEFAULT_ERROR_COLOR))}"
-        flashColorPref.summary = "#${argbToHex(readSecureInt(KEY_FLASH_COLOR, DEFAULT_FLASH_COLOR))}"
-        bgColorPref.summary = "#${argbToHex(readSecureInt(KEY_BG_COLOR, DEFAULT_BG_COLOR))}"
-    }
-
-    private fun readSecureInt(key: String, default: Int): Int =
-        Settings.Secure.getInt(requireContext().contentResolver, key, default)
-
-    private fun writeSecureInt(key: String, value: Int) {
-        Settings.Secure.putInt(requireContext().contentResolver, key, value)
+    private fun writeSystemInt(key: String, value: Int) {
+        Settings.System.putInt(requireContext().contentResolver, key, value)
     }
 
     private fun argbToHex(argb: Int): String =
