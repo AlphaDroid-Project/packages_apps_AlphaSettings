Lunaris-AOSP/packages_apps_Singularity

Adding statusbar dynamic bar
Ref and credits:
https://github.com/AxionAOSP/android_packages_apps_AxionParts/blob/lineage-23.2/src/com/android/axion/axionparts/ui/screens/DynamicBarScreen.kt

Rewrite and adapted for SettingsPreferenceFragment.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: GHOST | ゴースト <90827302+Ghosuto@users.noreply.github.com>

src/org/lunaris/settings/fragments/statusbar/DynamicBar.kt
@@ -1,0 +1,166 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lunaris.settings.fragments.statusbar
+
+import android.content.ContentResolver
+import android.content.Context
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.UserHandle
+import android.provider.Settings
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
+import androidx.preference.SwitchPreferenceCompat
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+import org.json.JSONArray
+
+class DynamicBar : SettingsPreferenceFragment() {
+
+    private val resolver: ContentResolver
+        get() = requireContext().contentResolver
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var settingsObserver: ContentObserver? = null
+
+    private val eventTypeIds = listOf(
+        "screen_recording",
+        "privacy",
+        "audio_recording",
+        "media",
+        "notification",
+        "timer",
+        "stopwatch",
+        "alarm",
+        "charging",
+        "bluetooth",
+        "hotspot",
+        "ringer",
+        "vpn",
+        "clipboard",
+        "torch",
+        "casting",
+        "promoted_ongoing",
+        "sports",
+        "app_switch",
+        "biometric_unlock",
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.dynamic_bar)
+
+        setupEventToggles()
+        updateCompactNotificationVisibility()
+        registerObserver()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        settingsObserver?.let { resolver.unregisterContentObserver(it) }
+    }
+
+    private fun setupEventToggles() {
+        val disabledEvents = getDisabledEvents()
+
+        for (typeId in eventTypeIds) {
+            val pref = findPreference<SwitchPreferenceCompat>("event_$typeId") ?: continue
+            pref.isChecked = typeId !in disabledEvents
+            pref.setOnPreferenceChangeListener { _, newValue ->
+                val enabled = newValue as Boolean
+                toggleEvent(typeId, enabled)
+                if (typeId == "notification") {
+                    updateCompactNotificationVisibility()
+                }
+                true
+            }
+        }
+    }
+
+    private fun getDisabledEvents(): Set<String> {
+        val json = Settings.Secure.getStringForUser(
+            resolver, SETTINGS_KEY_EVENTS, UserHandle.USER_CURRENT
+        ) ?: return emptySet()
+
+        if (json.isBlank()) return emptySet()
+
+        return try {
+            val arr = JSONArray(json)
+            (0 until arr.length()).mapTo(mutableSetOf()) { arr.getString(it) }
+        } catch (_: Exception) {
+            emptySet()
+        }
+    }
+
+    private fun toggleEvent(typeId: String, enabled: Boolean) {
+        val current = getDisabledEvents()
+        val updated = if (enabled) current - typeId else current + typeId
+        val json = if (updated.isEmpty()) "" else JSONArray(updated.toList()).toString()
+        Settings.Secure.putStringForUser(
+            resolver, SETTINGS_KEY_EVENTS, json, UserHandle.USER_CURRENT
+        )
+    }
+
+    private fun updateCompactNotificationVisibility() {
+        val compactPref = findPreference<Preference>(SETTINGS_KEY_COMPACT_NOTIFICATIONS)
+        val notifPref = findPreference<SwitchPreferenceCompat>("event_notification")
+        compactPref?.isVisible = notifPref?.isChecked == true
+    }
+
+    private fun registerObserver() {
+        settingsObserver = object : ContentObserver(handler) {
+            override fun onChange(selfChange: Boolean, uri: Uri?) {
+                when (uri?.lastPathSegment) {
+                    SETTINGS_KEY_EVENTS -> {
+                        val disabledEvents = getDisabledEvents()
+                        for (typeId in eventTypeIds) {
+                            val pref = findPreference<SwitchPreferenceCompat>("event_$typeId")
+                            pref?.isChecked = typeId !in disabledEvents
+                        }
+                        updateCompactNotificationVisibility()
+                    }
+                }
+            }
+        }
+
+        resolver.registerContentObserver(
+            Settings.Secure.getUriFor(SETTINGS_KEY_EVENTS),
+            false, settingsObserver!!
+        )
+    }
+
+    override fun getMetricsCategory(): Int {
+        return MetricsProto.MetricsEvent.LUNARIS
+    }
+
+    companion object {
+        private const val SETTINGS_KEY_ENABLED = "ax_dynamic_bar_enabled"
+        private const val SETTINGS_KEY_KEYGUARD_ENABLED = "ax_dynamic_bar_keyguard_enabled"
+        private const val SETTINGS_KEY_EVENTS = "ax_dynamic_bar_events"
+        private const val SETTINGS_KEY_COMPACT_NOTIFICATIONS = "ax_dynamic_bar_compact_notifications"
+
+        @JvmStatic
+        fun reset(context: Context) {
+            val resolver = context.contentResolver
+            Settings.Secure.putIntForUser(
+                resolver, SETTINGS_KEY_ENABLED, 0,
+                UserHandle.USER_CURRENT
+            )
+            Settings.Secure.putIntForUser(
+                resolver, SETTINGS_KEY_KEYGUARD_ENABLED, 1,
+                UserHandle.USER_CURRENT
+            )
+            Settings.Secure.putIntForUser(
+                resolver, SETTINGS_KEY_COMPACT_NOTIFICATIONS, 1,
+                UserHandle.USER_CURRENT
+            )
+        }
+    }
+}

src/org/lunaris/settings/fragments/statusbar/DynamicBarGuide.kt
@@ -1,0 +1,61 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lunaris.settings.fragments.statusbar
+
+import android.os.Bundle
+import android.widget.TextView
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+import com.android.settingslib.widget.LayoutPreference
+
+class DynamicBarGuide : SettingsPreferenceFragment() {
+
+    private data class GuideStep(val titleRes: Int, val bodyRes: Int)
+
+    private val guideSteps = listOf(
+        GuideStep(R.string.dynamic_bar_guide_chip_title, R.string.dynamic_bar_guide_chip),
+        GuideStep(R.string.dynamic_bar_guide_swipe_title, R.string.dynamic_bar_guide_swipe),
+        GuideStep(R.string.dynamic_bar_guide_expand_title, R.string.dynamic_bar_guide_expand),
+        GuideStep(R.string.dynamic_bar_guide_dismiss_title, R.string.dynamic_bar_guide_dismiss),
+        GuideStep(R.string.dynamic_bar_guide_keyguard_title, R.string.dynamic_bar_guide_keyguard),
+    )
+
+    private val stepViewIds = intArrayOf(
+        R.id.guide_step_1,
+        R.id.guide_step_2,
+        R.id.guide_step_3,
+        R.id.guide_step_4,
+        R.id.guide_step_5,
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.dynamic_bar_guide_prefs)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        populateGuideSteps()
+    }
+
+    private fun populateGuideSteps() {
+        val layoutPref = findPreference<LayoutPreference>("dynamic_bar_guide_content")
+            ?: return
+
+        guideSteps.forEachIndexed { index, step ->
+            val stepView = layoutPref.findViewById<android.view.View>(stepViewIds[index])
+                ?: return@forEachIndexed
+            stepView.findViewById<TextView>(R.id.step_number)?.text = "${index + 1}"
+            stepView.findViewById<TextView>(R.id.step_title)?.setText(step.titleRes)
+            stepView.findViewById<TextView>(R.id.step_body)?.setText(step.bodyRes)
+        }
+    }
+
+    override fun getMetricsCategory(): Int {
+        return MetricsProto.MetricsEvent.LUNARIS
+    }
+}

src/org/lunaris/settings/fragments/statusbar/DynamicBarKeyguardDemoView.kt
@@ -1,0 +1,152 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lunaris.settings.fragments.statusbar
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class DynamicBarKeyguardDemoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private var glowAlpha = 0.5f
+
+    private val chipPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.WHITE
+        textSize = dpToPx(12f)
+        typeface = android.graphics.Typeface.create("sans-serif-medium", android.graphics.Typeface.NORMAL)
+    }
+    private val dotPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = Color.WHITE }
+    private val btnBgPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val barPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = Color.WHITE }
+
+    private val chipPath = Path()
+    private val chipRect = RectF()
+
+    private val glowAnimator = ValueAnimator.ofFloat(0.5f, 1f).apply {
+        duration = 800
+        repeatMode = ValueAnimator.REVERSE
+        repeatCount = ValueAnimator.INFINITE
+        addUpdateListener {
+            glowAlpha = it.animatedValue as Float
+            invalidate()
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        glowAnimator.start()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        glowAnimator.cancel()
+    }
+
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if (visibility == VISIBLE && !glowAnimator.isStarted) glowAnimator.start()
+        else if (visibility != VISIBLE) glowAnimator.cancel()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val cx = width / 2f
+        val cy = height / 2f
+        val chipH = dpToPx(36f)
+
+        val timeText = "2:34"
+        val timeW = textPaint.measureText(timeText)
+
+        val padS = dpToPx(6f)
+        val padE = dpToPx(8f)
+        val dotR = dpToPx(5f)
+        val gap = dpToPx(6f)
+        val btnSize = dpToPx(24f)
+        val btnGap = dpToPx(4f)
+
+        val contentW = dotR * 2 + gap + timeW + gap + btnSize * 2 + btnGap
+        val chipW = padS + contentW + padE
+
+        val chipLeft = cx - chipW / 2f
+        val chipTop = cy - chipH / 2f
+
+        // Chip background
+        chipPaint.color = COLOR_RED
+        chipRect.set(chipLeft, chipTop, chipLeft + chipW, chipTop + chipH)
+        chipPath.reset()
+        chipPath.addRoundRect(chipRect, chipH / 2f, chipH / 2f, Path.Direction.CW)
+        canvas.drawPath(chipPath, chipPaint)
+
+        var x = chipLeft + padS
+
+        // Blinking recording dot
+        dotPaint.alpha = (glowAlpha * 255).toInt()
+        canvas.drawCircle(x + dotR, cy, dotR, dotPaint)
+        dotPaint.alpha = 255
+        x += dotR * 2 + gap
+
+        // Time text
+        val textY = cy - (textPaint.descent() + textPaint.ascent()) / 2f
+        canvas.drawText(timeText, x, textY, textPaint)
+        x += timeW + gap
+
+        // Pause button
+        btnBgPaint.color = Color.WHITE
+        btnBgPaint.alpha = (0.2f * 255).toInt()
+        canvas.drawCircle(x + btnSize / 2f, cy, btnSize / 2f, btnBgPaint)
+
+        val pauseBarW = dpToPx(3f)
+        val pauseBarH = dpToPx(10f)
+        barPaint.color = Color.WHITE
+        chipRect.set(
+            x + btnSize / 2f - pauseBarW - dpToPx(1f),
+            cy - pauseBarH / 2f,
+            x + btnSize / 2f - dpToPx(1f),
+            cy + pauseBarH / 2f
+        )
+        canvas.drawRoundRect(chipRect, dpToPx(1f), dpToPx(1f), barPaint)
+
+        chipRect.set(
+            x + btnSize / 2f + dpToPx(1f),
+            cy - pauseBarH / 2f,
+            x + btnSize / 2f + dpToPx(1f) + pauseBarW,
+            cy + pauseBarH / 2f
+        )
+        canvas.drawRoundRect(chipRect, dpToPx(1f), dpToPx(1f), barPaint)
+
+        x += btnSize + btnGap
+
+        // Stop button
+        canvas.drawCircle(x + btnSize / 2f, cy, btnSize / 2f, btnBgPaint)
+
+        val stopSize = dpToPx(8f)
+        chipRect.set(
+            x + btnSize / 2f - stopSize / 2f,
+            cy - stopSize / 2f,
+            x + btnSize / 2f + stopSize / 2f,
+            cy + stopSize / 2f
+        )
+        canvas.drawRoundRect(chipRect, dpToPx(2f), dpToPx(2f), barPaint)
+    }
+
+    private fun dpToPx(dp: Float): Float = dp * resources.displayMetrics.density
+
+    companion object {
+        private const val COLOR_RED = 0xFFFF453A.toInt()
+    }
+}

AlphaDroid-Project/packages_apps_AlphaSettings

src/com/alpha/settings/fragments/statusbar/DynamicBar.kt
@@ -1,0 +1,166 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.alpha.settings.fragments.statusbar
+
+import android.content.ContentResolver
+import android.content.Context
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.UserHandle
+import android.provider.Settings
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
+import androidx.preference.SwitchPreferenceCompat
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+import org.json.JSONArray
+
+class DynamicBar : SettingsPreferenceFragment() {
+
+    private val resolver: ContentResolver
+        get() = requireContext().contentResolver
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var settingsObserver: ContentObserver? = null
+
+    private val eventTypeIds = listOf(
+        "screen_recording",
+        "privacy",
+        "audio_recording",
+        "media",
+        "notification",
+        "timer",
+        "stopwatch",
+        "alarm",
+        "charging",
+        "bluetooth",
+        "hotspot",
+        "ringer",
+        "vpn",
+        "clipboard",
+        "torch",
+        "casting",
+        "promoted_ongoing",
+        "sports",
+        "app_switch",
+        "biometric_unlock",
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.dynamic_bar)
+
+        setupEventToggles()
+        updateCompactNotificationVisibility()
+        registerObserver()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        settingsObserver?.let { resolver.unregisterContentObserver(it) }
+    }
+
+    private fun setupEventToggles() {
+        val disabledEvents = getDisabledEvents()
+
+        for (typeId in eventTypeIds) {
+            val pref = findPreference<SwitchPreferenceCompat>("event_$typeId") ?: continue
+            pref.isChecked = typeId !in disabledEvents
+            pref.setOnPreferenceChangeListener { _, newValue ->
+                val enabled = newValue as Boolean
+                toggleEvent(typeId, enabled)
+                if (typeId == "notification") {
+                    updateCompactNotificationVisibility()
+                }
+                true
+            }
+        }
+    }
+
+    private fun getDisabledEvents(): Set<String> {
+        val json = Settings.Secure.getStringForUser(
+            resolver, SETTINGS_KEY_EVENTS, UserHandle.USER_CURRENT
+        ) ?: return emptySet()
+
+        if (json.isBlank()) return emptySet()
+
+        return try {
+            val arr = JSONArray(json)
+            (0 until arr.length()).mapTo(mutableSetOf()) { arr.getString(it) }
+        } catch (_: Exception) {
+            emptySet()
+        }
+    }
+
+    private fun toggleEvent(typeId: String, enabled: Boolean) {
+        val current = getDisabledEvents()
+        val updated = if (enabled) current - typeId else current + typeId
+        val json = if (updated.isEmpty()) "" else JSONArray(updated.toList()).toString()
+        Settings.Secure.putStringForUser(
+            resolver, SETTINGS_KEY_EVENTS, json, UserHandle.USER_CURRENT
+        )
+    }
+
+    private fun updateCompactNotificationVisibility() {
+        val compactPref = findPreference<Preference>(SETTINGS_KEY_COMPACT_NOTIFICATIONS)
+        val notifPref = findPreference<SwitchPreferenceCompat>("event_notification")
+        compactPref?.isVisible = notifPref?.isChecked == true
+    }
+
+    private fun registerObserver() {
+        settingsObserver = object : ContentObserver(handler) {
+            override fun onChange(selfChange: Boolean, uri: Uri?) {
+                when (uri?.lastPathSegment) {
+                    SETTINGS_KEY_EVENTS -> {
+                        val disabledEvents = getDisabledEvents()
+                        for (typeId in eventTypeIds) {
+                            val pref = findPreference<SwitchPreferenceCompat>("event_$typeId")
+                            pref?.isChecked = typeId !in disabledEvents
+                        }
+                        updateCompactNotificationVisibility()
+                    }
+                }
+            }
+        }
+
+        resolver.registerContentObserver(
+            Settings.Secure.getUriFor(SETTINGS_KEY_EVENTS),
+            false, settingsObserver!!
+        )
+    }
+
+    override fun getMetricsCategory(): Int {
+        return MetricsProto.MetricsEvent.ALPHA
+    }
+
+    companion object {
+        private const val SETTINGS_KEY_ENABLED = "ax_dynamic_bar_enabled"
+        private const val SETTINGS_KEY_KEYGUARD_ENABLED = "ax_dynamic_bar_keyguard_enabled"
+        private const val SETTINGS_KEY_EVENTS = "ax_dynamic_bar_events"
+        private const val SETTINGS_KEY_COMPACT_NOTIFICATIONS = "ax_dynamic_bar_compact_notifications"
+
+        @JvmStatic
+        fun reset(context: Context) {
+            val resolver = context.contentResolver
+            Settings.Secure.putIntForUser(
+                resolver, SETTINGS_KEY_ENABLED, 0,
+                UserHandle.USER_CURRENT
+            )
+            Settings.Secure.putIntForUser(
+                resolver, SETTINGS_KEY_KEYGUARD_ENABLED, 1,
+                UserHandle.USER_CURRENT
+            )
+            Settings.Secure.putIntForUser(
+                resolver, SETTINGS_KEY_COMPACT_NOTIFICATIONS, 1,
+                UserHandle.USER_CURRENT
+            )
+        }
+    }
+}

src/com/alpha/settings/fragments/statusbar/DynamicBarGuide.kt
@@ -1,0 +1,61 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.alpha.settings.fragments.statusbar
+
+import android.os.Bundle
+import android.widget.TextView
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+import com.android.settingslib.widget.LayoutPreference
+
+class DynamicBarGuide : SettingsPreferenceFragment() {
+
+    private data class GuideStep(val titleRes: Int, val bodyRes: Int)
+
+    private val guideSteps = listOf(
+        GuideStep(R.string.dynamic_bar_guide_chip_title, R.string.dynamic_bar_guide_chip),
+        GuideStep(R.string.dynamic_bar_guide_swipe_title, R.string.dynamic_bar_guide_swipe),
+        GuideStep(R.string.dynamic_bar_guide_expand_title, R.string.dynamic_bar_guide_expand),
+        GuideStep(R.string.dynamic_bar_guide_dismiss_title, R.string.dynamic_bar_guide_dismiss),
+        GuideStep(R.string.dynamic_bar_guide_keyguard_title, R.string.dynamic_bar_guide_keyguard),
+    )
+
+    private val stepViewIds = intArrayOf(
+        R.id.guide_step_1,
+        R.id.guide_step_2,
+        R.id.guide_step_3,
+        R.id.guide_step_4,
+        R.id.guide_step_5,
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.dynamic_bar_guide_prefs)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        populateGuideSteps()
+    }
+
+    private fun populateGuideSteps() {
+        val layoutPref = findPreference<LayoutPreference>("dynamic_bar_guide_content")
+            ?: return
+
+        guideSteps.forEachIndexed { index, step ->
+            val stepView = layoutPref.findViewById<android.view.View>(stepViewIds[index])
+                ?: return@forEachIndexed
+            stepView.findViewById<TextView>(R.id.step_number)?.text = "${index + 1}"
+            stepView.findViewById<TextView>(R.id.step_title)?.setText(step.titleRes)
+            stepView.findViewById<TextView>(R.id.step_body)?.setText(step.bodyRes)
+        }
+    }
+
+    override fun getMetricsCategory(): Int {
+        return MetricsProto.MetricsEvent.ALPHA
+    }
+}

src/com/alpha/settings/fragments/statusbar/DynamicBarKeyguardDemoView.kt
@@ -1,0 +1,152 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.alpha.settings.fragments.statusbar
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class DynamicBarKeyguardDemoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private var glowAlpha = 0.5f
+
+    private val chipPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.WHITE
+        textSize = dpToPx(12f)
+        typeface = android.graphics.Typeface.create("sans-serif-medium", android.graphics.Typeface.NORMAL)
+    }
+    private val dotPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = Color.WHITE }
+    private val btnBgPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val barPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = Color.WHITE }
+
+    private val chipPath = Path()
+    private val chipRect = RectF()
+
+    private val glowAnimator = ValueAnimator.ofFloat(0.5f, 1f).apply {
+        duration = 800
+        repeatMode = ValueAnimator.REVERSE
+        repeatCount = ValueAnimator.INFINITE
+        addUpdateListener {
+            glowAlpha = it.animatedValue as Float
+            invalidate()
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        glowAnimator.start()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        glowAnimator.cancel()
+    }
+
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if (visibility == VISIBLE && !glowAnimator.isStarted) glowAnimator.start()
+        else if (visibility != VISIBLE) glowAnimator.cancel()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val cx = width / 2f
+        val cy = height / 2f
+        val chipH = dpToPx(36f)
+
+        val timeText = "2:34"
+        val timeW = textPaint.measureText(timeText)
+
+        val padS = dpToPx(6f)
+        val padE = dpToPx(8f)
+        val dotR = dpToPx(5f)
+        val gap = dpToPx(6f)
+        val btnSize = dpToPx(24f)
+        val btnGap = dpToPx(4f)
+
+        val contentW = dotR * 2 + gap + timeW + gap + btnSize * 2 + btnGap
+        val chipW = padS + contentW + padE
+
+        val chipLeft = cx - chipW / 2f
+        val chipTop = cy - chipH / 2f
+
+        // Chip background
+        chipPaint.color = COLOR_RED
+        chipRect.set(chipLeft, chipTop, chipLeft + chipW, chipTop + chipH)
+        chipPath.reset()
+        chipPath.addRoundRect(chipRect, chipH / 2f, chipH / 2f, Path.Direction.CW)
+        canvas.drawPath(chipPath, chipPaint)
+
+        var x = chipLeft + padS
+
+        // Blinking recording dot
+        dotPaint.alpha = (glowAlpha * 255).toInt()
+        canvas.drawCircle(x + dotR, cy, dotR, dotPaint)
+        dotPaint.alpha = 255
+        x += dotR * 2 + gap
+
+        // Time text
+        val textY = cy - (textPaint.descent() + textPaint.ascent()) / 2f
+        canvas.drawText(timeText, x, textY, textPaint)
+        x += timeW + gap
+
+        // Pause button
+        btnBgPaint.color = Color.WHITE
+        btnBgPaint.alpha = (0.2f * 255).toInt()
+        canvas.drawCircle(x + btnSize / 2f, cy, btnSize / 2f, btnBgPaint)
+
+        val pauseBarW = dpToPx(3f)
+        val pauseBarH = dpToPx(10f)
+        barPaint.color = Color.WHITE
+        chipRect.set(
+            x + btnSize / 2f - pauseBarW - dpToPx(1f),
+            cy - pauseBarH / 2f,
+            x + btnSize / 2f - dpToPx(1f),
+            cy + pauseBarH / 2f
+        )
+        canvas.drawRoundRect(chipRect, dpToPx(1f), dpToPx(1f), barPaint)
+
+        chipRect.set(
+            x + btnSize / 2f + dpToPx(1f),
+            cy - pauseBarH / 2f,
+            x + btnSize / 2f + dpToPx(1f) + pauseBarW,
+            cy + pauseBarH / 2f
+        )
+        canvas.drawRoundRect(chipRect, dpToPx(1f), dpToPx(1f), barPaint)
+
+        x += btnSize + btnGap
+
+        // Stop button
+        canvas.drawCircle(x + btnSize / 2f, cy, btnSize / 2f, btnBgPaint)
+
+        val stopSize = dpToPx(8f)
+        chipRect.set(
+            x + btnSize / 2f - stopSize / 2f,
+            cy - stopSize / 2f,
+            x + btnSize / 2f + stopSize / 2f,
+            cy + stopSize / 2f
+        )
+        canvas.drawRoundRect(chipRect, dpToPx(2f), dpToPx(2f), barPaint)
+    }
+
+    private fun dpToPx(dp: Float): Float = dp * resources.displayMetrics.density
+
+    companion object {
+        private const val COLOR_RED = 0xFFFF453A.toInt()
+    }
+}