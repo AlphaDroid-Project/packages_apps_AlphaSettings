e649f642a538363d1d0239e7e2fc731ecd82e4a3
elpaablo <donopwhile1@gmail.com>
2026-04-16 11:53:13 +0100

    Add volume slider shapes

--- src/com/alpha/settings/fragments/Sound.java
+++ src/com/alpha/settings/fragments/Sound.java
@@ -107,6 +107,8 @@ public class Sound extends SettingsPreferenceFragment {
                 Settings.System.VIBRATE_ON_DISCONNECT, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.VOLUME_SLIDER_SHAPE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
