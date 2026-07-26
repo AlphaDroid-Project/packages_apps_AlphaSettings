67140447a8bb16d012b4065d7e1f82722e3352f9
elpaablo <donopwhile1@gmail.com>
2026-07-26 18:50:14 +0100

    QuickSettings: restore the QS tile shape picker, filtered to 8 shapes

--- src/com/alpha/settings/fragments/QuickSettings.java
+++ src/com/alpha/settings/fragments/QuickSettings.java
@@ -162,6 +162,8 @@ public class QuickSettings extends SettingsPreferenceFragment {
                 Settings.System.QS_SHOW_AUTO_BRIGHTNESS, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TILE_ANIMATION_STYLE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putStringForUser(resolver,
+                Settings.System.QS_TILE_ICON_SHAPE, "circle", UserHandle.USER_CURRENT);
         QsHeaderImageSettings.reset(mContext);
     }
 
